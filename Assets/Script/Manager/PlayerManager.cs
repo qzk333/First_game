3a7b666ca4e1b62e6997bd16130f131e971f6944
qzk333 <2632180794@qq.com>
2026-01-10 22:59:48 +0800

    修复存档bug

--- Assets/Script/Manager/PlayerManager.cs
+++ Assets/Script/Manager/PlayerManager.cs
@@ -17,11 +17,43 @@ public class PlayerManager : MonoBehaviour , ISaveManager
     
     public void LoadData(GameData _data)
     {
-        // TODO: 实现加载玩家数据的逻辑
+        // Ensure player reference is set
+        if (player == null)
+            player = FindObjectOfType<Player>();
+
+        if (player != null)
+        {
+            // Only load position if it's NOT the first time (to avoid snapping to 0,0,0 on New Game)
+            if (!_data.isFirstLoad)
+            {
+                player.transform.position = _data.playerPosition.ToVector3();
+            }
+            
+            // Restore health
+            if (player.stats != null)
+            {
+                player.stats.currentHealth = _data.playerHealth;
+                player.stats.maxHealth.SetDefaultValue(_data.playerMaxHealth);
+            }
+        }
     }
 
     public void SaveData(ref GameData _data)
     {
-        // TODO: 实现保存玩家数据的逻辑
+        if (player == null)
+            player = FindObjectOfType<Player>();
+
+        if (player != null)
+        {
+            // Save position
+            _data.playerPosition = SerializableVector3.FromVector3(player.transform.position);
+            
+            // Save health
+            if (player.stats != null)
+            {
+                _data.playerHealth = player.stats.currentHealth;
+                _data.playerMaxHealth = player.stats.maxHealth.GetValue();
+            }
+        }
     }
 }
